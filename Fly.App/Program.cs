23b9022c08b44d1a69f3a856b465988681cc4440
Daniel Morrison <iLogical@users.noreply.github.com>
2024-03-18 19:50:55 +0000

    Initial commit

---
+++ Fly.App/Program.cs
@@ -0,0 +1,4 @@
+﻿using Fly.Engine;
+
+using var game = new Game(800, 600, "Fly 0.0.1");
+game.Run();
---
+++ Fly.Engine/Game.cs
@@ -0,0 +1,31 @@
+﻿using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+namespace Fly.Engine;
+
+public sealed class Game(int width, int height, string title) : IDisposable
+{
+    private readonly IWindow _window = WindowManager.CreateWindow(width, height, title);
+
+    public void Dispose()
+    {
+        _window.Dispose();
+    }
+
+    public void Run()
+    {
+        _window.Run();
+    }
+}
+
+public static class WindowManager
+{
+    public static IWindow CreateWindow(int width, int height, string title = "")
+    {
+        var options = WindowOptions.Default;
+        options.Size = new Vector2D<int>(width, height);
+        options.Title = title;
+        options.API = GraphicsAPI.Default;
+        return Window.Create(options);
+    }
+}
